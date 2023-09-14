0d9d19761c2491d761a00afe221cdb60e9034c11
sergei <sergeicher87@gmail.com>
2023-09-14 14:00:39 +0300

    JavaScript

---
+++ js/main.js
@@ -0,0 +1,2 @@
+document.write("<h2>First program on JavaScript</h2>"); // print header
+document.write("Hello World!");                             // print regular text
---
+++ src/DOM.js
@@ -0,0 +1,5 @@
+import "../styles/styles.css";
+ 
+
+
+document.body.style.backgroundColor = "#dc143c";
